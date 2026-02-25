4c247fdffb1b9e9a5e20340b366a87af12881e5e
desiboli <adam.benals@gmail.com>
2026-02-26 00:07:09 +0100

    minor ui stuff

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -43,23 +43,23 @@ export function Pricing() {
       <div className="container mx-auto max-w-7xl px-4">
         <h2 className="text-fluid-4xl font-bold font-display text-center">Våra abonnemang</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mt-20">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10 md:mt-20">
           {pricing.map((item) => (
-            <div key={item.name} className={cn("bg-card rounded-xl p-10 flex flex-col justify-between", item.featured && "bg-secondary text-secondary-foreground")}>
-              <h3 className="text-fluid-lg">{item.name}</h3>
-              <p className="text-fluid-2xl font-bold">{Intl.NumberFormat('sv-SE').format(item.price)} kr/mån</p>
+            <div key={item.name} className={cn("bg-card/30 rounded-xl p-10 flex flex-col justify-between", item.featured && "bg-secondary text-secondary-foreground")}>
+              <h3 className={cn("text-fluid-lg", item.featured && "text-fluid-2xl")}>{item.name}</h3>
+              <p className={cn("text-fluid-2xl font-bold", item.featured && "text-fluid-3xl")}>{Intl.NumberFormat('sv-SE').format(item.price)} kr/mån</p>
 
               <div className="flex flex-col gap-2 mt-10">
                 {item.features.map((feature) => (
                   <div key={feature} className="flex items-center gap-2">
-                    <BadgeCheckIcon className="w-4 h-4" />
+                    <BadgeCheckIcon className={cn("size-4", item.featured && "text-primary")} />
                     <p className="">{feature}</p>
                   </div>
                 ))}
               </div>
 
 
-              <Button variant={item.featured ? "default" : "secondary"} className="w-full mt-20">
+              <Button variant={item.featured ? "default" : "secondary"} size={item.featured ? "lg" : "default"} className="w-full mt-20">
                 Kom igång
               </Button>
             </div>
