b8359741a40138ee6ba1cbea2fb9beda1bbbb9dc
desiboli <adam.benals@gmail.com>
2026-02-25 16:26:44 +0100

    hero container

--- src/components/client-logos.tsx
+++ src/components/client-logos.tsx
@@ -1,6 +1,6 @@
 export function ClientLogos() {
   return (
-    <div className="container mx-auto max-w-7xl -mt-20">
+    <div className="container mx-auto max-w-7xl -mt-20 px-4">
       <h2 className="text-sm text-center text-foreground/70">Företag vi har arbetat med</h2>
 
       <div className="flex flex-wrap items-center justify-center gap-20 mt-10">
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export function Hero() {
   return (
     <section className="bg-background h-screen flex items-center justify-center max-h-svh">
-      <div className="max-w-7xl flex flex-col items-center justify-center gap-6">
+      <div className="max-w-7xl flex flex-col items-center justify-center gap-6 container mx-auto px-4">
         <h1 className="text-fluid-6xl uppercase leading-none tracking-tight font-bold font-display text-center">Modern webb och AI utan krångel</h1>
         <p className="text-fluid-lg text-center">
           Transparent månadspris från 4 900 kr.
